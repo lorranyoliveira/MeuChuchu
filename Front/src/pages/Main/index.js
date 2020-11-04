f6841c028e0ef66c16e1bab10f92f08741708205
dani_so_o <dannysoaresdeoliveira@gmail.com>
2020-11-04 00:14:41 -0300

    Criando rotas

--- Front/src/pages/Main/index.js
+++ Front/src/pages/Main/index.js
@@ -7,27 +7,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  title1: {
-    fontFamily: 'Avenir-L',
-    fontSize: 20,
-  },
-  title2: {
-    fontFamily: 'Avenir-S',
-    fontSize: 20,
-  },
-  title3: {
-    fontFamily: 'Avenir-B',
-    fontSize: 20,
-  },
+  }
 });
 
 export default function Main() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title1}>Avenir-L</Text>
-      <Text style={styles.title2}>Avenir-S</Text>
-      <Text style={styles.title3}>Avenir-B</Text>
-    </View>
+    <View style={styles.container}/>
+
+   
   );
 }
