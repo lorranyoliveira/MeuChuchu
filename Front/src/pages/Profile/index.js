018509296d3ef740eb03f060e0613dd64edf4a17
lorrany oliveira <lolo.souza80@yahoo.com>
2020-11-05 15:11:10 -0300

    #11-Criando Página Profile

---
+++ Front/src/pages/Profile/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+
+
+
+const Profile = () => {
+    return (
+        <View>
+            <Text style = {styles.Nome}></Text>
+        </View>
+    );
+};
