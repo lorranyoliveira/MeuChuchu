760ffd8f7a1c0e80e921319f3a9dff4cb12489da
giovanadionisio <giovanadionisio.gd@gmail.com>
2020-11-11 01:06:01 -0300

    #15 - CriandoComponentHelpButton

---
+++ Front/src/components/HelpButton/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import styles from './styles';
+import { Ionicons } from '@expo/vector-icons';
+
+//declaração do componente botão
+
+const HelpButton = ({}) => (
+    <TouchableOpacity>
+      <View>
+        <Ionicons name="ios-help" size = {100} style={styles.button}/>
+      </View>
+    </TouchableOpacity>
+  );
+
+  export default HelpButton;
---
+++ Front/src/components/HelpButton/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet} from 'react-native';
+import { colors, fonts, metrics} from '../../styles';
+
+
+//declaração da estilização do botão
+
+const styles = StyleSheet.create({
+  button: {
+    color: colors.primaryGreen,
+    top: 11,
+    right: -170,
+  }
+
+});
+
+export default styles;
