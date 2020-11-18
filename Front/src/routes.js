2f0e1d258165246bfdcf456327183a89d1df3b36
lorrany oliveira <lolo.souza80@yahoo.com>
2020-11-18 17:11:28 -0300

    #15- Finalizando a tela de login

--- Front/src/routes.js
+++ Front/src/routes.js
@@ -6,12 +6,13 @@ const AppStack = createStackNavigator();
 //import Main from'./pages/Main';
 //import ProfileOption from './pages/ProfileOption';
 import Profile from './pages/Profile';
+import Login from './pages/Login';
 
 export default function Routes(){
     return(
         <NavigationContainer>
             <AppStack.Navigator screenOptions={{headerShown: false}}>
-                <AppStack.Screen name="Profile" component={Profile}/>
+                <AppStack.Screen name="Login" component={Login}/>
             </AppStack.Navigator>
         </NavigationContainer>
 
