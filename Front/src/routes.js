574467b0d71f2118bbc9f3272744d443b1ae2491
dani_so_o <dannysoaresdeoliveira@gmail.com>
2020-11-29 20:18:50 -0300

    21-Arrumando conflitos

--- Front/src/routes.js
+++ Front/src/routes.js
@@ -5,10 +5,11 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
 const AppStack = createStackNavigator();
-import Main from'./pages/Main';
+//import Main from'./pages/Main';
 //import ProfileOption from './pages/ProfileOption';
 //import Profile from './pages/Profile';
 //import ViewStand from './pages/ViewStand';
+import Login from './pages/Login';
 
 //declaração das páginas de navegação
 
@@ -16,7 +17,7 @@ export default function Routes(){
     return(
         <NavigationContainer>
             <AppStack.Navigator screenOptions={{headerShown: false}}>
-                <AppStack.Screen name="Main" component={Main}/>
+                <AppStack.Screen name="Login" component={Login}/>
             </AppStack.Navigator>
         </NavigationContainer>
 
