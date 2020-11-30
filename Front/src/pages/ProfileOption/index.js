5d307db227b177e522e57b08e0fd70fbf6be04a8
lorrany oliveira <lolo.souza80@yahoo.com>
2020-11-30 20:08:37 -0300

    #25-Tentanto interligar telas

--- Front/src/pages/ProfileOption/index.js
+++ Front/src/pages/ProfileOption/index.js
@@ -9,10 +9,14 @@ import Button from '../../components/Button';
 import HelpButton from '../../components/HelpButton';
 import { colors } from '../../styles';
 import styles from './styles';
+import { useNavigation } from '@react-navigation/native';
+import Profile from '../Profile';
+
 
 // declaração da estrutura da tela ProfileOption 
 
 const ProfileOption = () => {
+    const navigation = useNavigation();
     return (
         <View style={styles.card}>
             <Text style={styles.text}>Escolha o tipo de perfil que deseja acessar</Text>
@@ -21,11 +25,14 @@ const ProfileOption = () => {
                     text="Pessoal"
                     width={250}
                     backgroundColor= {colors.primaryGreen}
+                    onPress={()=>navigation.navigate('Profile')}
                 />
                 <Button
                     text="Comercial"
                     width={250}
                     backgroundColor= {colors.primaryGreen}
+                    
+                    
                 />
             </View>
         </View>
--- Front/src/routes.js
+++ Front/src/routes.js
@@ -13,83 +13,95 @@ import ProfileOption from './pages/ProfileOption';
 import Profile from './pages/Profile';
 import Search from './pages/Search';
 
-const NavBar = createStackNavigator();
 
-const NavScreen = () => {
-  return (
-    <NavBar.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: colors.btnBlue,
-          height: metrics.screenHeight * 0.13,
-        },
-        headerTitleAlign: 'center',
-        headerTitleStyle: {
-          color: colors.primaryYellow,
-          fontSize: fonts.title * 0.825,
-        },
-        headerTintColor: colors.primaryYellow,
-      }}
-    >
-      <NavBar.Screen
-        name="ProfileOption"
-        component={ProfileOption}
-        options={{
-          title: 'Tipo de Perfil',
-          headerRight: () => (
-            <TouchableOpacity
-              onPress={() => { sethelpModalVisible(!helpModalVisible); }}
-              style={{ margin: metrics.baseMargin * 2 }}
-            >
-              <FontAwesome name="question" size={32} color={colors.primaryGreen} />
-            </TouchableOpacity>
-          ),
-        }}
-      />
-    </NavBar.Navigator>
-  );
-};
+const HomeStack = createStackNavigator();
+const HomeScreen = () => (
+  <HomeStack.Navigator>
+    {/* <HomeStack.Screen name = "Starter" component={Starter}/> */}
+    <HomeStack.Screen name = "Escolha de Perfil" component={ProfileOption}/>
+    {/* <HomeStack.Screen name = "Login" component={Login}/> */}
+    <HomeStack.Screen name = "Profile"component={Profile}/>
+    <HomeStack.Screen name = "Search" component={Search}/>
+  </HomeStack.Navigator>
+)
 
-const NavSerch = createStackNavigator();
+// const NavBar = createStackNavigator();
 
-const NavSerchScreen = () => {
-  return (
-    <NavSerch.Navigator
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: colors.btnBlue,
-          height: metrics.screenHeight * 0.13,
-        },
-        headerTitleAlign: 'center',
-        headerTitleStyle: {
-          color: colors.primaryYellow,
-          fontSize: fonts.title * 0.825,
-        },
-        headerTintColor: colors.primaryYellow,
-      }}
-    >
-      <NavSerch.Screen
-        name="Search"
-        component={Search}
-        options={{
-          title: 'Buscar',
-          headerRight: () => (
-            <TouchableOpacity
-              onPress={() => { sethelpModalVisible(!helpModalVisible); }}
-              style={{ margin: metrics.baseMargin * 2 }}
-            >
-              <FontAwesome name="question" size={32} color={colors.primaryGreen} />
-            </TouchableOpacity>
-          ),
-        }}
-      />
-    </NavSerch.Navigator>
-  );
-};
-const AppStack = createMaterialBottomTabNavigator();
+// const NavScreen = () => {
+//   return (
+//     <NavBar.Navigator
+//       screenOptions={{
+//         headerStyle: {
+//           backgroundColor: colors.btnBlue,
+//           height: metrics.screenHeight * 0.13,
+//         },
+//         headerTitleAlign: 'center',
+//         headerTitleStyle: {
+//           color: colors.primaryYellow,
+//           fontSize: fonts.title * 0.825,
+//         },
+//         headerTintColor: colors.primaryYellow,
+//       }}
+//     >
+//       <NavBar.Screen
+//         name="ProfileOption"
+//         component={ProfileOption}
+//         options={{
+//           title: 'Tipo de Perfil',
+//           headerRight: () => (
+//             <TouchableOpacity
+//               onPress={() => { sethelpModalVisible(!helpModalVisible); }}
+//               style={{ margin: metrics.baseMargin * 2 }}
+//             >
+//               <FontAwesome name="question" size={32} color={colors.primaryGreen} />
+//             </TouchableOpacity>
+//           ),
+//         }}
+//       />
+//     </NavBar.Navigator>
+//   );
+// };
+
+// const NavSerch = createStackNavigator();
+
+// const NavSerchScreen = () => {
+//   return (
+//     <NavSerch.Navigator
+//       screenOptions={{
+//         headerStyle: {
+//           backgroundColor: colors.btnBlue,
+//           height: metrics.screenHeight * 0.13,
+//         },
+//         headerTitleAlign: 'center',
+//         headerTitleStyle: {
+//           color: colors.primaryYellow,
+//           fontSize: fonts.title * 0.825,
+//         },
+//         headerTintColor: colors.primaryYellow,
+//       }}
+//     >
+//       <NavSerch.Screen
+//         name="Search"
+//         component={Search}
+//         options={{
+//           title: 'Buscar',
+//           headerRight: () => (
+//             <TouchableOpacity
+//               onPress={() => { sethelpModalVisible(!helpModalVisible); }}
+//               style={{ margin: metrics.baseMargin * 2 }}
+//             >
+//               <FontAwesome name="question" size={32} color={colors.primaryGreen} />
+//             </TouchableOpacity>
+//           ),
+//         }}
+//       />
+//     </NavSerch.Navigator>
+//   );
+// };
+// const AppStack = createMaterialBottomTabNavigator();
 
 export default function Routes() {
-  const [isReady, setReady] = useState(false);
+//   const [isReady, setReady] = useState(false);
 
 //   const netInfo = useNetInfo();
 //   const { signed } = useAuth();
@@ -102,34 +114,39 @@ export default function Routes() {
 
 //  if (netInfo.isConnected) {
 //    if (signed) {
+      // return (
+      //   <NavigationContainer>
+      //     <AppStack.Navigator
+      //       headerMode="none"
+      //       activeColor={colors.selected}
+      //       inactiveColor={colors.gray}
+      //       barStyle={{ backgroundColor: colors.primaryGreen}}
+      //       shifting={false}
+      //     >
+      //       <AppStack.Screen
+      //         name="Busca"
+      //         component={NavSerchScreen}
+      //         options={{
+      //           tabBarIcon: ({ color }) => (
+      //             <AntDesign name="search1" size={24} color={color} />
+      //           ),
+      //         }}
+      //       />
+      //       <AppStack.Screen
+      //         name="Perfil"
+      //         component={NavScreen}
+      //         options={{
+      //           tabBarIcon: ({ color }) => (
+      //             <AntDesign name="user" size={24} color={color} />
+      //           ),
+      //         }}
+      //       />
+      //     </AppStack.Navigator>
+      //   </NavigationContainer>
+      // );
       return (
         <NavigationContainer>
-          <AppStack.Navigator
-            headerMode="none"
-            activeColor={colors.selected}
-            inactiveColor={colors.gray}
-            barStyle={{ backgroundColor: colors.primaryGreen}}
-            shifting={false}
-          >
-            <AppStack.Screen
-              name="Busca"
-              component={NavSerchScreen}
-              options={{
-                tabBarIcon: ({ color }) => (
-                  <AntDesign name="search1" size={24} color={color} />
-                ),
-              }}
-            />
-            <AppStack.Screen
-              name="Perfil"
-              component={NavScreen}
-              options={{
-                tabBarIcon: ({ color }) => (
-                  <AntDesign name="user" size={24} color={color} />
-                ),
-              }}
-            />
-          </AppStack.Navigator>
+          <HomeScreen />
         </NavigationContainer>
       );
     }
